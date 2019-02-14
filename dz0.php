78cc5bbcac5f8155fa191578dbc26057c0e3064f
Alexander Velichko <alvi@mail.ua>
2019-02-14 13:02:41 +0200

    fix: fix 'missing file doc comment'

--- dz0.php
+++ dz0.php
@@ -1,7 +1,38 @@
 <?php
-CONST testConstanta = 'asd';
+/**
+ * Generic_Sniffs_Methods_OpeningMethodBraceBsdAllmanSniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   CVS: $Id: OpeningFunctionBraceBsdAllmanSniff.php,v 1.8
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+/**
+ * There is some constants
+ *
+ * @file dz0.php
+ */
+
+CONST TEST_CONSTANTA = 'asd';
 $user_name = 'Igor';
 
-function show_something() {RETURN 'SOMETHING';}
+/**
+ * Description.
+ *
+ * @return string
+ */
+function Show_something()
+{
+    return 'SOMETHING';
+}
 
-if(1==1) echo 'hi';
+if (1==1) {
+    echo 'hi';
+}
